915b2a4dbb9f895fd338ad75c7cdb1ca0f0852ce
olyme <olyme111@naver.com>
2025-05-13 15:46:17 +0900

    AI에 입력할 json Data

---
+++ output_AI_json.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+from adc_mcp3008 import MCP3008
+from temperature_humidity import TemperatureHumiditySensor
+from soil_moisture_pump import SoilMoisturePump
+from light_sensor_led import LightSensorLED
+
+app = Flask(__name__)
+
+@app.route('/measure', methods=['GET'])
+def measure_sensors():
+    # 센서 초기화
+    adc = MCP3008()
+    temp_humid_sensor = TemperatureHumiditySensor(pin=5)
+
+    # 센서 값 읽기
+    temperature, humidity = temp_humid_sensor.read_data()
+
+    # 센서 데이터를 JSON 형식으로 준비
+    sensor_data = {
+        'temperature': temperature,
+        'humidity': humidity,
+    }
+
+    # JSON 형식으로 데이터 반환
+    return jsonify(sensor_data)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
